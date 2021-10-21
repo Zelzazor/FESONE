614bd5752a17bbb9fc6db4ebe09bad79797b44f6
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-10-21 02:14:44 -0400

    Implementación función putProjects

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { Projects } from './Projects';
+
+
+
+const ManipulateDOM = (() => {
+    const putProjects = () => {
+        const projectsDOM = document.querySelector('.list-projects');
+        
+        Projects.getAllProjects().forEach((project, index) => {
+            let projectDOM = document.createElement("div");
+            projectDOM.dataset.id = index;
+            projectDOM.classList.add("project");
+            let titleDOM = document.createElement("p");
+            titleDOM.classList.add("title");
+            titleDOM.textContent = project.title;
+            let descDOM = document.createElement("p");
+            descDOM.classList.add("description");
+            descDOM.textContent = project.description;
+            projectDOM.appendChild(titleDOM);
+            projectDOM.appendChild(descDOM);
+            projectDOM.addEventListener("click", () => {
+                let projectDetails = document.querySelector(".project-details");
+                let todos = document.querySelector(".todos");
+
+                while (projectDetails.firstChild) {
+                    projectDetails.removeChild(projectDetails.lastChild);
+                }
+                while (todos.firstChild) {
+                    todos.removeChild(todos.lastChild);
+                }
+                projectDetails.appendChild(ProjectFieldsDOM(project, index));
+                todos.appendChild(todosDOM(index));
+                todos.appendChild(barDOM(index));
+            })
+            projectsDOM.appendChild(projectDOM);
+        });
+
+    }
+})();
